8765cd9e45fca17a6bc07d463a40544eba6865e2
jonneale <jonneale@gmail.com>
2015-05-07 10:04:47 +0100

    Added some personal customizations

---
+++ packs/jonneale/jonneale-pack/init.el
@@ -0,0 +1,36 @@
+;; User pack init file
+;;
+;; Use this file to initiate the pack configuration.
+;; See README for more information.
+
+;; Load bindings config
+(live-load-config-file "bindings.el")
+(defun toggle-fullscreen ()
+  "Toggle full screen"
+  (interactive)
+  (set-frame-parameter
+     nil 'fullscreen
+     (when (not (frame-parameter nil 'fullscreen)) 'fullboth)))
+     
+(global-set-key [f7] 'toggle-fullscreen)
+
+(when (fboundp 'windmove-default-keybindings)
+      (windmove-default-keybindings))
+
+(defun beautify-json ()
+  (interactive)
+  (let ((b (if mark-active (min (point) (mark)) (point-min)))
+        (e (if mark-active (max (point) (mark)) (point-max))))
+    (shell-command-on-region b e
+                             "python -mjson.tool" (current-buffer) t)
+    (esk-indent-buffer)))
+
+(require 'nrepl)
+ 
+(defun nrepl-limit-print-length ()
+  (interactive)
+  (nrepl-send-string-sync "(set! *print-length* 100)" "clojure.core"))
+ 
+(defun nrepl-unlimit-print-length ()
+  (interactive)
+  (nrepl-send-string-sync "(set! *print-length* nil)" "clojure.core"))
